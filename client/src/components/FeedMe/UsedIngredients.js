d6d25d00c11e8a8893350fcb2d61739632f8fbcc
Glib Lakeev <glakeev@gmail.com>
2023-04-12 11:13:19 -0400

    search used ingredients decimal trim

--- client/src/components/FeedMe/UsedIngredients.js
+++ client/src/components/FeedMe/UsedIngredients.js
@@ -9,13 +9,31 @@ const UsedIngredients = ({ usedIngredients }) => {
   // console.log(ingredientSearchQuery)
 
   // console.log(usedIngredients.usedIngredients)
+
+
   
   return (
     <Wrapper>
       {usedIngredients.usedIngredients.map((ing) => {
+
+        let shortAmount = ing.amount
+
+          //count decimals
+        const countDecimals = (value) => {
+          if (Math.floor(value) === value) return 0;
+          return value.toString().split(".")[1].length || 0;
+          }
+
+        //shorten amount if decimals is > 2
+          if (countDecimals(shortAmount) > 2) {
+             shortAmount = ing.amount.toFixed(2)
+          }
+
+        // else, return full ing.amount
+
         return (
           <span key={ing.id}>
-            <span> {ing.name} ({ing.amount} {ing.unit}) </span>
+            <span> {ing.name} ({shortAmount} {ing.unit}) </span>
 
           </span>
         );
